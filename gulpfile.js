60d3bf6c00350cba6d3c335deacb0284b1b46581
eden lane <edencore@gmail.com>
2015-04-16 16:49:38 +0300

    updated

--- gulpfile.js
+++ gulpfile.js
@@ -60,8 +60,7 @@ gulp.task('watch', function () {
 gulp.task('ghPages', function () {
     return gulp.src('./demo/**/*')
         .pipe(ghPages({
-            remoteUrl: 'git@github.com:dvhb/dvhb-subwaymap.git',
-            push: false
+            remoteUrl: 'git@github.com:dvhb/dvhb-subwaymap.git'
         }))
 });
 
